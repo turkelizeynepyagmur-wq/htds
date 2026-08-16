e56ca2d070f1539e9ba94f99dde62240697cc8c3
Zeynep Yağmur Türkeli <turkelizeynepyagmur@gmail.com>
2026-08-16 17:56:28 +0300

    Fix: Test dosyasındaki isValid() metodu gecerliMi() olarak düzeltildi

--- src/test/java/com/hastane/htds/util/TcKimlikValidatorTest.java
+++ src/test/java/com/hastane/htds/util/TcKimlikValidatorTest.java
@@ -8,23 +8,23 @@ class TcKimlikValidatorTest {
     @Test
     void testValidTcKimlik() {
         // DatabaseSeeder içinde kullanılan ve algoritmayı geçen test TC Kimlik Numarası
-        assertTrue(TcKimlikValidator.isValid("10000000146"), "Geçerli TC kimlik doğrulamadan geçmeli");
+        assertTrue(TcKimlikValidator.gecerliMi("10000000146"), "Geçerli TC kimlik doğrulamadan geçmeli");
     }
 
     @Test
     void testInvalidLength() {
-        assertFalse(TcKimlikValidator.isValid("1234567890"), "10 haneli TC geçersiz sayılmalı");
-        assertFalse(TcKimlikValidator.isValid("123456789012"), "12 haneli TC geçersiz sayılmalı");
+        assertFalse(TcKimlikValidator.gecerliMi("1234567890"), "10 haneli TC geçersiz sayılmalı");
+        assertFalse(TcKimlikValidator.gecerliMi("123456789012"), "12 haneli TC geçersiz sayılmalı");
     }
 
     @Test
     void testInvalidCharacters() {
-        assertFalse(TcKimlikValidator.isValid("1234567890A"), "Harf içeren TC geçersiz sayılmalı");
+        assertFalse(TcKimlikValidator.gecerliMi("1234567890A"), "Harf içeren TC geçersiz sayılmalı");
     }
 
     @Test
     void testNullOrEmpty() {
-        assertFalse(TcKimlikValidator.isValid(null), "Null değer geçersiz sayılmalı");
-        assertFalse(TcKimlikValidator.isValid(""), "Boş string geçersiz sayılmalı");
+        assertFalse(TcKimlikValidator.gecerliMi(null), "Null değer geçersiz sayılmalı");
+        assertFalse(TcKimlikValidator.gecerliMi(""), "Boş string geçersiz sayılmalı");
     }
 }
